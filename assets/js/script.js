ef3d875ac308efa38db13cb47fcc99e2cde27aae
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-07-31 18:23:18 -0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+//Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
