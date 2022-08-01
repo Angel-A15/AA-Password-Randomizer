812ef2764f52cc159c8e59492941663b32857980
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-08-01 18:12:09 -0500

    linked the html to the css and js, fixed error

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,19 @@
 //Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("hey! You touched the button!")
+
+  //1. Prompted for password criteria
+  //  a: Password length
+  //  b: lowercase, uppercase, numbers, special symbols
+  //2. Validate the input
+  //3. Generate password based on criteria
+  //4. Display password
+
+  return "Generated password will go here!"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
